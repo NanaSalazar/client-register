2322fd3ee0290bef0d23e021a9fbf859ffb4077a
Ana Salazar <102234649+NanaSalazar@users.noreply.github.com>
2023-01-25 19:44:45 -0300

    att

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const clients = require("./data.json")
 
+
 const app = express()
 
 app.use(express.json())
@@ -8,6 +9,7 @@ app.use(express.urlencoded( {extended: true}))
 
 app.get("/back/data", (req, res) => {
     res.send(clients)
+    console.log(email, pass)
 })
 
 app.post("/back/data", (req, res) => {
---
+++ front/app.js
@@ -0,0 +1,13 @@
+
+const login = document.querySelector('#ilogon')
+
+login.addEventListener('click', loginClient)
+
+function loginClient() {
+    const email = document.querySelector('#iemail').value
+    const pass = document.querySelector('#ipass').value
+
+
+}
+
+
